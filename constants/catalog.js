5b683f1beae5aff0e0cfd3c22faa6117bb170912
SuperShprot <densours@yandex.ru>
2024-03-25 22:08:53 +0500

    Update catalog.js

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name:'ST Classic 004',
+        img:'https://khmelevskyguitars.com/wa-data/public/shop/products/55/00/55/images/404/404.300x0.jpg',
+        price: 60000,
+    },
+    {
+        id: 'el2',
+        name:'TL deluxe 111',
+        img:'https://khmelevskyguitars.com/wa-data/public/shop/products/65/00/65/images/533/533.750.jpg',
+        price: 58000,
+    },
+    {
+        id: 'el3',
+        name:'TL deluxe 106',
+        img:'https://khmelevskyguitars.com/wa-data/public/shop/products/66/00/66/images/541/541.750.jpg',
+        price: 65900,
+    },
+];
